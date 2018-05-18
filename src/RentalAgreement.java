adbf3d08abc439c863b4eefb77822ec3048b9565
TristanIsStout <tristanstoutenburg@gmail.com>
2018-05-17 17:07:47 -0700

    Implemented equals method for rental agreements

--- src/RentalAgreement.java
+++ src/RentalAgreement.java
@@ -12,9 +12,22 @@ public class RentalAgreement {
   public String discountAmount;
   public String finalCharge;
 
-  public static boolean equal(RentalAgreement rentalAgrement1,
+  public static boolean equal(RentalAgreement rentalAgreement1,
                               RentalAgreement rentalAgreement2) {
-    return false;
+    // if both are null, they are equal
+    if (rentalAgreement1 == null && rentalAgreement2 == null) {
+      return true;
+    } else if (rentalAgreement1 == null || rentalAgreement2 == null) {
+      return false;
+    }
+    // if all properties are the same, they are equal
+    return (rentalAgreement1.dueDate ==rentalAgreement2.dueDate &&
+            rentalAgreement1.dailyCharge ==rentalAgreement2.dailyCharge &&
+            rentalAgreement1.chargeDays ==rentalAgreement2.chargeDays &&
+            rentalAgreement1.preDiscountCharge ==rentalAgreement2.preDiscountCharge &&
+            rentalAgreement1.discountPercent ==rentalAgreement2.discountPercent &&
+            rentalAgreement1.discountAmount ==rentalAgreement2.discountAmount &&
+            rentalAgreement1.finalCharge ==rentalAgreement2.finalCharge);
   }
 
 }
